bcf1c3833698681618ae44e65869ab670905ebde
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-19 19:11:47 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -1,46 +1,53 @@
 import pandas as pd
-import openpyxl
-from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 
-# Read the two excel files and store them in dataframes
-df1 = pd.read_excel("file1.xlsx")
-df2 = pd.read_excel("file2.xlsx")
+# Function to read data from a worksheet and return a DataFrame
+def read_data(sheet, start_row):
+    data = []
+    for row in sheet.iter_rows(min_row=start_row, values_only=True):
+        data.append(row)
+    return pd.DataFrame(data, columns=data[0])
 
-# Get the original order of columns
-original_columns_order = list(df1.columns)
+# Function to compare two DataFrames and highlight the differences in the Excel file
+def highlight_differences(df1, df2, sheet, fill_color="FFFF00"):
+    for col in df1.columns:
+        for row in df1.index:
+            if df1.at[row, col] != df2.at[row, col]:
+                cell = sheet.cell(row=row + df1_start_row, column=df1.columns.get_loc(col) + 1)
+                cell.fill = PatternFill(start_color=fill_color, end_color=fill_color, fill_type="solid")
 
-# Align DataFrames based on original order of columns
-df1, df2 = df1.align(df2, axis=1, join='outer', fill_value=None)
+# Load the workbook and select the first sheet
+file_path = 'path/to/your/file.xlsx'  # Update with the actual file path
+wb = load_workbook(file_path)
+sheet = wb.active
 
-# Reorder columns to match the original order
-df1 = df1[original_columns_order]
-df2 = df2[original_columns_order]
+# Find the positions of Q02 and Q03 in the sheet
+q02_position = None
+q03_position = None
 
-# Find the differences between the two dataframes
-diff = df1.compare(df2, keep_equal=True)
+for row in sheet.iter_rows(values_only=True):
+    if "Q02" in row:
+        q02_position = row
+    elif "Q03" in row:
+        q03_position = row
 
-# Create a new dataframe to store the full data from file1 and file2
-df3 = pd.concat([df1.astype(str), df2.astype(str)], ignore_index=True)
+# Read data into DataFrames
+if q02_position and q03_position:
+    # Find the positions to split the data
+    q02_start_row = sheet.cell(row=sheet.min_row, column=sheet.max_column).row + 2
+    q03_start_row = sheet.cell(row=sheet.min_row, column=sheet.max_column).row + 1
 
-# Load the new dataframe as a workbook object
-wb = openpyxl.Workbook()
-ws = wb.active
+    # Read Q02 data
+    df_q02 = read_data(sheet, q02_start_row)
 
-# Write the dataframe to the workbook
-for r in dataframe_to_rows(df3, index=False, header=True):
-    ws.append([str(cell) for cell in r])
+    # Read Q03 data
+    df_q03 = read_data(sheet, q03_start_row)
 
-# Define a style for highlighting the cells
-highlight = openpyxl.styles.PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+    # Highlight differences
+    highlight_differences(df_q02, df_q03, sheet)
 
-# Loop through the differences and apply the highlight style to the corresponding cells in the workbook
-for index, row in diff.iterrows():
-    for col, value in row.items():
-        try:
-            cell = ws.cell(row=index + 2, column=int(col) + 1)
-            cell.fill = highlight
-        except ValueError:
-            print(f"Skipping invalid index: row={index}, col={col}")
-
-# Save the workbook as a new file
-wb.save("file3.xlsx")
+    # Save the workbook
+    wb.save('path/to/your/output_file.xlsx')  # Update with the desired output file path
+else:
+    print("Q02 or Q03 not found in the sheet.")
