4a9212828a4f3e1f4b34a2b755d9050d1cb3c11e
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-19 16:57:25 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -1,41 +1,23 @@
-import os
+# Import pandas library
 import pandas as pd
 
-def compare_and_merge(folder1, folder2, output_folder):
-    # Step 1: Get a list of all Excel files in folder1 and folder2
-    excel_files_folder1 = [f for f in os.listdir(folder1) if f.endswith('.xlsx')]
-    excel_files_folder2 = [f for f in os.listdir(folder2) if f.endswith('.xlsx')]
+# Read the two excel files
+df1 = pd.read_excel("file1.xlsx")
+df2 = pd.read_excel("file2.xlsx")
 
-    # Step 2: Compare and merge Excel files
-    for file1 in excel_files_folder1:
-        if file1 in excel_files_folder2:
-            print(f"Processing file: {file1}")
+# Compare the values in the two dataframes
+comparison_values = df1.values == df2.values
 
-            try:
-                # Load Excel files into DataFrames
-                df1 = pd.read_excel(os.path.join(folder1, file1))
-                df2 = pd.read_excel(os.path.join(folder2, file1))
-                
-                # Align DataFrames based on columns and indexes
-                df1, df2 = df1.align(df2, axis=1, join='outer')
-                df1, df2 = df1.align(df2, axis=0, join='outer')
+# Find the rows and columns where the values are different
+rows, cols = np.where(comparison_values == False)
 
-            except Exception as e:
-                print(f"Error reading files {file1}: {e}")
-                continue
+# Create a new dataframe to store the merged data
+df3 = df1.copy()
 
-            # Compare and highlight differences
-            df_diff = df1.compare(df2)
+# Loop through the cells that are different and update the values in df3
+# Use a format like "value1 --> value2" to show the changes
+for item in zip(rows, cols):
+    df3.iloc[item[0], item[1]] = "{} --> {}".format(df1.iloc[item[0], item[1]], df2.iloc[item[0], item[1]])
 
-            # Step 3: Save the merged DataFrame to a new Excel file in the output folder
-            output_file_path = os.path.join(output_folder, f'merged_{file1}')
-            df_diff.to_excel(output_file_path, sheet_name='Differences', index=False)
-
-            print(f"File saved to: {output_file_path}")
-
-if __name__ == "__main__":
-    folder1 = r'C:\data\old\folder1'
-    folder2 = r'C:\data\old\folder2'
-    output_folder = r'C:\data\old\folder3'
-
-    compare_and_merge(folder1, folder2, output_folder)
+# Save the merged dataframe as a new excel file
+df3.to_excel("file3.xlsx")
