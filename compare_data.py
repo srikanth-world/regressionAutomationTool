411ac49354ada1e3d80a15a74a290253833694dc
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-19 13:45:10 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -7,33 +7,36 @@ def compare_and_merge_excel(file1_path, file2_path, columns_to_compare, file3_pa
 
     # Initialize an Excel writer for the output file
     with pd.ExcelWriter(file3_path, engine='openpyxl') as writer:
-        # Use option_context to temporarily change the default visibility behavior
-        with pd.option_context('openpyxl.writer.excel.save.visible', False):
-            # Iterate over each sheet in both Excel files
-            for sheet_name in excel1.sheet_names:
-                # Read data from the current sheet in both files
-                df1 = excel1.parse(sheet_name)
-                df2 = excel2.parse(sheet_name)
+        # Manually set visibility for a dummy sheet
+        writer.book.create_sheet('dummy_sheet')
+        writer.book['dummy_sheet'].sheet_state = 'hidden'
+        writer.book.remove(writer.book['dummy_sheet'])
 
-                # Compare dataframes based on specified columns
-                merged_data = pd.merge(df1, df2, on=columns_to_compare, how='outer', suffixes=('_file1', '_file2'), indicator=True)
+        # Iterate over each sheet in both Excel files
+        for sheet_name in excel1.sheet_names:
+            # Read data from the current sheet in both files
+            df1 = excel1.parse(sheet_name)
+            df2 = excel2.parse(sheet_name)
 
-                # Identify added, updated, and deleted rows
-                added_rows = merged_data.loc[lambda x: x['_merge'] == 'right_only']
-                updated_rows = merged_data.loc[lambda x: x['_merge'] == 'both']
-                deleted_rows = merged_data.loc[lambda x: x['_merge'] == 'left_only']
+            # Compare dataframes based on specified columns
+            merged_data = pd.merge(df1, df2, on=columns_to_compare, how='outer', suffixes=('_file1', '_file2'), indicator=True)
 
-                # Highlight differences in the data
-                differences_styled = pd.DataFrame(index=merged_data.index)
-                differences_styled.loc[added_rows.index, :] = 'background: lightgreen'
-                differences_styled.loc[updated_rows.index, :] = 'background: lightyellow'
+            # Identify added, updated, and deleted rows
+            added_rows = merged_data.loc[lambda x: x['_merge'] == 'right_only']
+            updated_rows = merged_data.loc[lambda x: x['_merge'] == 'both']
+            deleted_rows = merged_data.loc[lambda x: x['_merge'] == 'left_only']
 
-                # Merge data from both files into a new dataframe
-                merged_data = pd.merge(df1, df2, on=columns_to_compare, how='outer', suffixes=('_file1', '_file2'))
+            # Highlight differences in the data
+            differences_styled = pd.DataFrame(index=merged_data.index)
+            differences_styled.loc[added_rows.index, :] = 'background: lightgreen'
+            differences_styled.loc[updated_rows.index, :] = 'background: lightyellow'
 
-                # Write the differences and merged data to the output file with individual sheets
-                differences_styled.to_excel(writer, sheet_name=f'Differences_{sheet_name}', index=False, header=False, engine='openpyxl', startrow=1)
-                merged_data.to_excel(writer, sheet_name=f'MergedData_{sheet_name}', index=False)
+            # Merge data from both files into a new dataframe
+            merged_data = pd.merge(df1, df2, on=columns_to_compare, how='outer', suffixes=('_file1', '_file2'))
+
+            # Write the differences and merged data to the output file with individual sheets
+            differences_styled.to_excel(writer, sheet_name=f'Differences_{sheet_name}', index=False, header=False, engine='openpyxl', startrow=1)
+            merged_data.to_excel(writer, sheet_name=f'MergedData_{sheet_name}', index=False)
 
 # Example usage
 file1_path = 'path/to/file1.xlsx'
