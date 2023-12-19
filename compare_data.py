761d34c93f25a28155595f889ffebc03b23f685a
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-19 14:09:54 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -9,6 +9,14 @@ def compare_and_merge(file1_path, file2_path, columns_to_compare, output_file_pa
 
     # Create a Pandas Excel writer using XlsxWriter as the engine
     with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
+        # Write an empty dataframe to create the Excel file
+        pd.DataFrame().to_excel(writer, sheet_name='dummy', index=False)
+
+        # Get the xlsxwriter workbook and worksheet objects
+        workbook = writer.book
+        worksheet = workbook['dummy']
+        workbook.remove(worksheet)  # Remove the dummy sheet
+
         # Iterate through all sheets in both files
         for sheet_name in set(df1.keys()) | set(df2.keys()):
             # Get the dataframes for the current sheet (or empty dataframe if sheet not present)
@@ -18,30 +26,18 @@ def compare_and_merge(file1_path, file2_path, columns_to_compare, output_file_pa
             # Merge dataframes on specified columns
             merged_df = pd.merge(sheet_df1, sheet_df2, how='outer', on=columns_to_compare, indicator=True)
 
-            # Create a Pandas Excel writer for the current sheet
+            # Write the merged dataframe to the Excel file
             merged_df.to_excel(writer, sheet_name=sheet_name, index=False, startrow=1, header=False)
 
-            # Get the xlsxwriter workbook and worksheet objects
-            workbook = writer.book
-            worksheet = writer.sheets[sheet_name]
-
-            # Ensure the sheet is visible
-            workbook[sheet_name].sheet_state = 'visible'
-
-            # Get the xlsxwriter writer objects
-            xlsxwriter_writer = pd.ExcelWriter(output_file_path, engine='openpyxl')
-            xlsxwriter_workbook = xlsxwriter_writer.book
-            xlsxwriter_worksheet = xlsxwriter_writer.sheets[sheet_name]
+            # Get the xlsxwriter worksheet object for the current sheet
+            xlsxwriter_worksheet = workbook[sheet_name]
 
             # Define cell formats and highlight differences
             fill = PatternFill(start_color="yellow", end_color="yellow", fill_type="solid")
-            for row in range(1, len(merged_df) + 2):
-                if worksheet.cell(row=row, column=len(merged_df.columns) + 3).value == 'both':
+            for row in range(2, len(merged_df) + 2):
+                if xlsxwriter_worksheet.cell(row=row, column=len(merged_df.columns) + 1).value == 'both':
                     for col in range(1, len(merged_df.columns) + 1):
                         xlsxwriter_worksheet.cell(row=row, column=col).fill = fill
-
-            # Close the Pandas Excel writer for the current sheet
-            xlsxwriter_writer.save()
 
 # Example usage
 compare_and_merge('file1.xlsx', 'file2.xlsx', ['Column1', 'Column2'], 'file3.xlsx')
