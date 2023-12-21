b5ecf600ecf24d5f06ba69a6714daff94122e717
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-21 22:46:21 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -14,18 +14,20 @@ def compare_and_merge(path1, path2, output_path):
         file2_path = os.path.join(path2, file)
 
         # Load Excel files into pandas dataframes
-        df1 = pd.read_excel(file1_path, engine='openpyxl')
-        df2 = pd.read_excel(file2_path, engine='openpyxl')
+        df1 = pd.read_excel(file1_path, engine='openpyxl', sheet_name=None)
+        df2 = pd.read_excel(file2_path, engine='openpyxl', sheet_name=None)
 
         # Find differences between dataframes
-        diff = pd.concat([df1, df2]).drop_duplicates(keep=False)
+        diff = pd.concat([df1[key] for key in df1] + [df2[key] for key in df2]).drop_duplicates(keep=False)
 
         # Highlight differences in the merged dataframe
         diff_styled = diff.style.applymap(lambda x: 'background-color: yellow', subset=pd.IndexSlice[:, :])
 
         # Write the merged and highlighted dataframe to a new Excel file
         with pd.ExcelWriter(os.path.join(output_path, f'Merged_{file}'), engine='openpyxl') as writer:
-            diff_styled.to_excel(writer, index=False, sheet_name='Merged')
+            for sheet_name, sheet_df in diff.items():
+                sheet_df.to_excel(writer, index=False, sheet_name=sheet_name)
+                writer.sheets[sheet_name].sheet_view.tabSelected = True
 
 if __name__ == "__main__":
     # Replace these paths with your actual paths
