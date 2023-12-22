7241d4b631c6ad455129eb024d4f5e807e326945
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-22 12:18:51 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -15,8 +15,8 @@ def compare_and_merge(path1, path2, output_path):
         file2_path = os.path.join(path2, file)
 
         # Load Excel files into pandas dataframes
-        df1 = pd.read_excel(file1_path, engine='openpyxl', sheet_name=None, header=1)
-        df2 = pd.read_excel(file2_path, engine='openpyxl', sheet_name=None, header=1)
+        df1 = pd.read_excel(file1_path, engine='openpyxl', sheet_name=None, header=0)
+        df2 = pd.read_excel(file2_path, engine='openpyxl', sheet_name=None, header=0)
 
         # Create a new workbook
         merged_workbook = Workbook()
@@ -27,41 +27,29 @@ def compare_and_merge(path1, path2, output_path):
             sheet_df1 = df1.get(sheet_name, pd.DataFrame())
             sheet_df2 = df2.get(sheet_name, pd.DataFrame())
 
-            # Ensure both DataFrames have the same indices and columns
-            common_index = sheet_df1.index.union(sheet_df2.index)
-            common_columns = sheet_df1.columns.union(sheet_df2.columns)
-
-            sheet_df1 = sheet_df1.reindex(index=common_index, columns=common_columns, fill_value=None)
-            sheet_df2 = sheet_df2.reindex(index=common_index, columns=common_columns, fill_value=None)
-
-            # Identify differences
-            diff_cells = (sheet_df1.values != sheet_df2.values)
-
-            # Merge old and new data
-            merged_df = sheet_df1.combine_first(sheet_df2)
-
             # Create a new sheet in the merged workbook
             merged_sheet = merged_workbook.create_sheet(title=sheet_name)
 
-            # Write the header row
-            header_row = list(merged_df.columns)
-            merged_sheet.append(header_row)
-
-            # Write data from the first dataset
-            for row in dataframe_to_rows(sheet_df1, index=False, header=False):
+            # Write data from the first dataset with header
+            merged_sheet.append([f'\n{sheet_name} from File 1'])
+            for row in dataframe_to_rows(sheet_df1, index=False, header=True):
                 merged_sheet.append(row)
 
             # Write an empty row as a separator
             merged_sheet.append([])
 
-            # Write data from the second dataset
-            for row in dataframe_to_rows(sheet_df2, index=False, header=False):
+            # Write data from the second dataset with header
+            merged_sheet.append([f'\n{sheet_name} from File 2'])
+            for row in dataframe_to_rows(sheet_df2, index=False, header=True):
                 merged_sheet.append(row)
 
-            # Apply styling to highlight differences
-            for row_idx, row in enumerate(merged_sheet.iter_rows(min_row=3, max_row=merged_sheet.max_row, min_col=1, max_col=merged_sheet.max_column), start=3):
+            # Identify differences
+            diff_cells = (sheet_df1.values != sheet_df2.values)
+
+            # Highlight differences
+            for row_idx, row in enumerate(merged_sheet.iter_rows(min_row=4, max_row=merged_sheet.max_row, min_col=1, max_col=merged_sheet.max_column), start=4):
                 for col_idx, cell in enumerate(row, start=1):
-                    if diff_cells[row_idx - 3, col_idx - 1]:  # Adjust indices for 0-based indexing
+                    if diff_cells[row_idx - 4, col_idx - 1]:
                         cell.fill = openpyxl.styles.PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
 
         # Remove the default sheet created by openpyxl
