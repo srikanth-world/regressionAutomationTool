cb8cdd65d42fc2fba72973558381c16e93980f6c
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-12-22 12:53:07 +0530

    Update compare_data.py

--- compare_data.py
+++ compare_data.py
@@ -15,8 +15,8 @@ def compare_and_merge(path1, path2, output_path):
         file2_path = os.path.join(path2, file)
 
         # Load Excel files into pandas dataframes
-        df1 = pd.read_excel(file1_path, engine='openpyxl', sheet_name=None, header=0)
-        df2 = pd.read_excel(file2_path, engine='openpyxl', sheet_name=None, header=0)
+        df1 = pd.read_excel(file1_path, engine='openpyxl', sheet_name=None, header=None)
+        df2 = pd.read_excel(file2_path, engine='openpyxl', sheet_name=None, header=None)
 
         # Create a new workbook
         merged_workbook = Workbook()
@@ -27,29 +27,35 @@ def compare_and_merge(path1, path2, output_path):
             sheet_df1 = df1.get(sheet_name, pd.DataFrame())
             sheet_df2 = df2.get(sheet_name, pd.DataFrame())
 
+            # Skip if both dataframes are empty
+            if sheet_df1.empty and sheet_df2.empty:
+                continue
+
             # Create a new sheet in the merged workbook
             merged_sheet = merged_workbook.create_sheet(title=sheet_name)
 
             # Write data from the first dataset with header
-            merged_sheet.append([f'\n{sheet_name} from File 1'])
-            for row in dataframe_to_rows(sheet_df1, index=False, header=True):
-                merged_sheet.append(row)
+            if not sheet_df1.empty:
+                merged_sheet.append([f'\n{sheet_name} from File 1'])
+                for row in dataframe_to_rows(sheet_df1, index=False, header=True):
+                    merged_sheet.append(row)
 
             # Write an empty row as a separator
             merged_sheet.append([])
 
             # Write data from the second dataset with header
-            merged_sheet.append([f'\n{sheet_name} from File 2'])
-            for row in dataframe_to_rows(sheet_df2, index=False, header=True):
-                merged_sheet.append(row)
+            if not sheet_df2.empty:
+                merged_sheet.append([f'\n{sheet_name} from File 2'])
+                for row in dataframe_to_rows(sheet_df2, index=False, header=True):
+                    merged_sheet.append(row)
 
             # Identify differences
             diff_cells = (sheet_df1.values != sheet_df2.values)
 
             # Highlight differences
-            for row_idx, row in enumerate(merged_sheet.iter_rows(min_row=4, max_row=merged_sheet.max_row, min_col=1, max_col=merged_sheet.max_column), start=4):
+            for row_idx, row in enumerate(merged_sheet.iter_rows(min_row=len(merged_sheet._cells) + 2, max_row=merged_sheet.max_row, min_col=1, max_col=merged_sheet.max_column), start=len(merged_sheet._cells) + 2):
                 for col_idx, cell in enumerate(row, start=1):
-                    if diff_cells[row_idx - 4, col_idx - 1]:
+                    if diff_cells[row_idx - len(merged_sheet._cells) - 2, col_idx - 1]:
                         cell.fill = openpyxl.styles.PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
 
         # Remove the default sheet created by openpyxl
